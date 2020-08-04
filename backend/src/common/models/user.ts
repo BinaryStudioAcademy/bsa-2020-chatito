b9a9a240173a70242be93ac7657333f2f93d9941
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-08-04 17:42:55 +0300

    add register user flow.

--- backend/src/common/models/user.ts
+++ backend/src/common/models/user.ts
@@ -1,3 +1,19 @@
 export interface IAuthUser {
   id: string;
 }
+
+export interface IRegisterUser {
+  fullName: string;
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  id: string,
+  fullName: string,
+  email: string,
+  password: string,
+  displayName: string,
+  imageUrl: string,
+  title: string
+}
