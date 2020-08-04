569359d3583a79cf9d2333d378f4974741acbd1a
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-08-04 10:53:00 +0300

    react included

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,5 +2,6 @@ import './styles/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
