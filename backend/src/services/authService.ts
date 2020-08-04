b8ea43e3814f616dde8750cd22e329026dd91c3f
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-08-04 18:24:46 +0300

    add access token to response on register.

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,6 +1,7 @@
 import userRepository from '../data/repositories/userRepository';
 import { IRegisterUser } from '../common/models/user';
 import { encrypt } from '../common/utils/encryptHelper';
+import { createToken } from '../common/utils/tokenHelper';
 
 export const register = async ({ password, ...userData }: IRegisterUser) => {
   const newUser = await userRepository.addUser({
@@ -10,6 +11,6 @@ export const register = async ({ password, ...userData }: IRegisterUser) => {
 
   return {
     ...newUser,
-    token: 'token'
+    token: createToken({ id: newUser.id })
   };
 };
