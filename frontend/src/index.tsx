81aad2bd0922f471d7afd605c9fec2e5a94c21ca
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-08-04 11:24:43 +0300

    versios changed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
