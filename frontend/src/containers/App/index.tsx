daec364281f4c8ded1ed724e998ef7d8be3c6923
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-04 20:10:39 +0300

    refactored code

--- frontend/src/containers/App/index.tsx
+++ frontend/src/containers/App/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
 import { ConnectedRouter } from 'connected-react-router';
-import { history } from "../../common/helpers/historyHelper"
+import { history } from '../../common/helpers/historyHelper';
 
 const App = () => (
   <Provider store={store}>
-      <ConnectedRouter history={history}>
+    <ConnectedRouter history={history}>
       <span>Welcome to Chatito!</span>
-      </ConnectedRouter>
+    </ConnectedRouter>
   </Provider>
 );
 
