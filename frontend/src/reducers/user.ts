00b964ee47d467ab04371244641aa59d166ef18f
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-04 17:10:28 +0300

    create user state

--- frontend/src/reducers/user.ts
+++ frontend/src/reducers/user.ts
@@ -1,6 +1,14 @@
 import { Routine } from 'redux-saga-routines';
 
-export default (state = {}, action:Routine<any>) => {
+export interface IUserState {
+  loading:boolean
+}
+
+const initialState:IUserState = {
+  loading: false
+};
+
+export default (state = initialState, action: Routine<any>) => {
   switch (action.type) {
     default:
       return state;
