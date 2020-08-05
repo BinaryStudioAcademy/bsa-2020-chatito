d0c3f0157e11d6fb0a0ecbd4afa0b546f4dbe5a8
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-05 18:15:46 +0300

    create editUser method in repository

--- backend/src/data/repositories/userRepository.ts
+++ backend/src/data/repositories/userRepository.ts
@@ -2,12 +2,24 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import { User } from '../entities/UserEntity';
 import { ICreateUser } from '../../common/models/user/ICreateUser';
+import { IUserClient } from '../../common/models/user/IUserClient';
 
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
   async addUser(data: ICreateUser): Promise<User> {
     const user = this.create(data);
     await user.save();
+
+    return user;
+  }
+
+  async editUser(id:string, data: IUserClient): Promise<User> {
+    await this.update(
+      id,
+      data
+    );
+
+    const user = await this.findOne(id);
 
     return user;
   }
