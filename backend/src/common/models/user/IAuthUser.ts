26c7ec7207b5c1454b2b14db4fcaf446baef54cd
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-05 22:04:56 +0300

    delete unused code

--- backend/src/common/models/user/IAuthUser.ts
+++ backend/src/common/models/user/IAuthUser.ts
@@ -2,22 +2,3 @@ export interface IAuthUser {
   id: string;
 }
 
-export interface IRegisterUser {
-  fullName: string;
-  email: string;
-  password: string;
-}
-
-export interface IUser {
-  id: string,
-  fullName: string,
-  email: string,
-  password: string,
-  displayName: string,
-  imageUrl: string,
-  title: string
-}
-
-export interface IUserResponse extends IUser {
-  token: string,
-}
