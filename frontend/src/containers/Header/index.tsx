2c965b5aa6f93354dfc4ec20bb34d601e602cba7
Yaroslav Klymenko <yaroslaw.klymko@gmail.com>
2020-08-05 22:01:31 +0300

    branch naming mistake (#21)
    * search input added
    
    * redux connected
    
    * function refactor

--- frontend/src/containers/Header/index.tsx
+++ frontend/src/containers/Header/index.tsx
@@ -1,18 +1,23 @@
 import styles from './styles.module.sass';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { connect } from 'react-redux';
 import UserAvatar from '../../components/UserLogo';
 import UserPopUp from '../../components/UserPopUp';
-import { IUser } from '../../common/models/user';
+import SearchInput from '../../components/SearchInput';
+import { IAppState } from '../../common/models/store';
+import { IUserState } from '../../reducers/user';
 
-const defaultAvatar = 'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg'; // eslint-disable-line max-len
-const testUser: IUser = {
-  imgUrl: defaultAvatar,
-  fullname: 'YareK'
-};
+interface IProps {
+  user: IUserState;
+}
 
-const Header = () => {
+const Header: FunctionComponent<IProps> = ({ user }) => {
   const toggleButtonClick = () => {
     // @todo decide which button to trigger
+  };
+
+  const testUser = {
+    imgUrl: 'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg' // eslint-disable-line max-len
   };
 
   const getAvatar = () => (
@@ -22,7 +27,7 @@ const Header = () => {
       tabIndex={0}
       onKeyDown={toggleButtonClick}
     >
-      <UserAvatar imgUrl={defaultAvatar} isOnline />
+      <UserAvatar imgUrl={testUser.imgUrl} isOnline />
     </div>
   );
 
@@ -30,12 +35,18 @@ const Header = () => {
     <header className={styles.headerContainer}>
       <div className={styles.mainLogo}>Logo</div>
 
-      <div>Some components here</div>
-      <UserPopUp user={testUser} trigger={getAvatar} id="mainHeaderPopUp" placement="bottom" />
+      <SearchInput onSearch={t => console.log('Search for ', t)} stylesClassName={styles.searchInput} />
+
+      {user.isAuthorized
+        ? <UserPopUp user={user} trigger={getAvatar} id="mainHeaderPopUp" placement="bottom" />
+        : <div>You need to sign in</div>}
 
     </header>
   );
 };
 
+const mapStateToProps = (state: IAppState) => ({ user: state.user });
 
-export default Header;
+export default connect(
+  mapStateToProps
+)(Header);
