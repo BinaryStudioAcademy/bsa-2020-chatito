7eb2b8f8a653f7835fbcc7e2c8d3f7e96ea0c369
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-08-05 20:09:34 +0300

    redux connected

--- frontend/src/components/UserPopUp/index.tsx
+++ frontend/src/components/UserPopUp/index.tsx
@@ -5,21 +5,28 @@ import { PopUpPosition } from '../../common/enums/ComponentProps';
 import Image from 'react-bootstrap/Image';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import { IUserState } from '../../reducers/user';
 
 interface IProps {
-  user: IUser;
+  user: IUserState;
   trigger: () => React.ReactElement;
   id: string;
   placement: PopUpPosition;
 }
 
 const UserPopUp: FunctionComponent<IProps> = ({ user, trigger, id, placement }) => {
+  console.log(user);
+  const testUser = {
+    fullname: 'test fullname',
+    imgUrl: 'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg' // eslint-disable-line max-len
+  };
+
   const popOver = (
     <Popover id={id}>
-      <Image className={styles.userAvatarBig} src={user.imgUrl} alt="User avatar big" thumbnail />
+      <Image className={styles.userAvatarBig} src={testUser.imgUrl} alt="User avatar big" thumbnail />
 
       <Popover.Content>
-        <p className={styles.usernameHeader}>{user.fullname}</p>
+        <p className={styles.usernameHeader}>{testUser.fullname}</p>
         <p>Some info</p>
         <p>Some othe info</p>
       </Popover.Content>
