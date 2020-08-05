e70ebaca51b28a3cbfd98d5bfd871c908e037d9e
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-05 15:55:55 +0300

    delete actions

--- frontend/src/reducers/actions/actionTypes.ts
+++
@@ -1,1 +0,0 @@
-export const ADD_WORKSPACE = 'ADD_WORKSPACE';
--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { history } from '../common/helpers/historyHelper';
 import UserReducer from './user';
-import workspaceReducer from './workspaceReducer';
 
 const rootReducer = () => combineReducers({
   router: connectRouter(history),
   UserReducer,
-  workspaceReducer
 });
 
 export default rootReducer;
