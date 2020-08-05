ecab445291e404961fdb8f9064bc9a9f292ceb86
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-05 19:09:38 +0300

    add store Edit Profile to IAppState

--- frontend/src/common/models/store.ts
+++ frontend/src/common/models/store.ts
@@ -1,7 +1,9 @@
 import { IUserState } from '../../reducers/user';
 import { RouterState } from 'connected-react-router';
+import { IEditProfileState } from '../../containers/EditProfile/reducer';
 
 export interface IAppState {
   router: RouterState;
   user: IUserState;
+  editUserProfile: IEditProfileState
 }
