7c167dee4b8d1645690ac9859caa46b026158767
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-05 18:21:51 +0300

    create service for editing profile

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,3 +1,8 @@
+import { getCustomRepository } from 'typeorm';
+import { IUserClient } from '../common/models/user/IUserClient';
+import UserRepository from '../data/repositories/userRepository';
+import { fromUserToUserClient } from '../common/mappers/user';
+
 const users = [{
   id: '1',
   name: 'Alik'
@@ -9,3 +14,10 @@ const users = [{
 export const getUsers = () => Promise.resolve(users);
 
 export const getUserById = (id: string) => Promise.resolve(users.find(u => u.id === id));
+
+export const editProfile = async (user: IUserClient) => {
+  const editUser = await getCustomRepository(UserRepository).editUser(user.id, user);
+  return {
+    user: fromUserToUserClient(editUser)
+  };
+};
