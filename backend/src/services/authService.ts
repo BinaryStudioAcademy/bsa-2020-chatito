b33fb97f0a92bbef2f4e7cb7d17007c235208be1
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-06 22:53:57 +0300

    Implement tokens route

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,10 +1,13 @@
 import { getCustomRepository } from 'typeorm';
 
 import UserRepository from '../data/repositories/userRepository';
+import RefreshTokenRepository from '../data/repositories/refreshTokenRepository';
+
 import { IRegisterUser } from '../common/models/user/IRegisterUser';
 import { encrypt } from '../common/utils/encryptHelper';
 import { fromUserToUserClient, fromRegisterUserToCreateUser } from '../common/mappers/user';
-import { createToken } from '../common/utils/tokenHelper';
+import { createToken, createRefreshTokenData } from '../common/utils/tokenHelper';
+import { encryptString, decryptString } from '../common/utils/cryptrHelper';
 
 export const register = async ({ password, ...userData }: IRegisterUser) => {
   const passwordHash = await encrypt(password);
@@ -12,8 +15,40 @@ export const register = async ({ password, ...userData }: IRegisterUser) => {
 
   const newUser = await getCustomRepository(UserRepository).addUser(createUserData);
 
+  const refreshTokenData = createRefreshTokenData(newUser.id);
+  const refreshToken = await getCustomRepository(RefreshTokenRepository).addToken(refreshTokenData);
+
   return {
     user: fromUserToUserClient(newUser),
-    token: createToken({ id: newUser.id })
+    accessToken: createToken({ id: newUser.id }),
+    refreshToken: encryptString(refreshToken.id)
   };
+};
+
+export const refreshTokens = async (encryptedId: string) => {
+  try {
+    const id = decryptString(encryptedId);
+    const refreshTokenRepository = getCustomRepository(RefreshTokenRepository);
+
+    const refreshToken = await refreshTokenRepository.getById(id);
+
+    if (!refreshToken) {
+      throw Error('Invalid token');
+    } else if (Date.now() > refreshToken.expiresAt) {
+      await refreshTokenRepository.deleteToken(id);
+      throw Error('Token expired');
+    }
+
+    const newRefreshTokenData = createRefreshTokenData(refreshToken.userId);
+    const newRefreshToken = await refreshTokenRepository.addToken(newRefreshTokenData);
+
+    await refreshTokenRepository.deleteToken(id);
+
+    return {
+      accessToken: createToken({ id: newRefreshToken.userId }),
+      refreshToken: encryptString(newRefreshToken.id)
+    };
+  } catch (err) {
+    throw Error('Internal error');
+  }
 };
