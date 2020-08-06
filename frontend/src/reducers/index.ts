cb478dbd8557c67ca1235d3194e6f9bc76a480b1
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-06 11:34:55 +0300

    Adding toolbar

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { history } from '../common/helpers/historyHelper';
-import UserReducer from './user';
 import workspace from '../scenes/Workspace/reducer';
 import user from './user';
 
