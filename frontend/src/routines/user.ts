6efffb8f0d6b4d08668e3cda0334bdf544fb4d0d
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-06 19:50:31 +0300

    rename routine

--- frontend/src/routines/user.ts
+++ frontend/src/routines/user.ts
@@ -2,6 +2,6 @@ import { createRoutine } from 'redux-saga-routines';
 
 const addNewUserRoutine = createRoutine('ADD_NEW_USER');
 const fetchUserRoutine = createRoutine('FETCH_USER');
-const editProfile = createRoutine('EDIT_PROFILE');
+const editProfileRoutine = createRoutine('EDIT_PROFILE');
 
-export { addNewUserRoutine, fetchUserRoutine, editProfile };
+export { addNewUserRoutine, fetchUserRoutine, editProfileRoutine };
