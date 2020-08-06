d58ab273810de3c86058222856677850738d93c5
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-06 18:07:07 +0300

    connect modal reducer to root reducer

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -3,11 +3,13 @@ import { connectRouter } from 'connected-react-router';
 import { history } from '../common/helpers/historyHelper';
 import workspace from '../scenes/Workspace/reducers/reducer';
 import user from './user';
+import modal from './modal';
 
 const rootReducer = () => combineReducers({
   router: connectRouter(history),
   user,
-  workspace
+  workspace,
+  modal
 });
 
 export default rootReducer;
