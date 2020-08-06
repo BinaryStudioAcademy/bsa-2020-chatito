c095234293e32e22786719844180a6eac61a8eb6
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-06 19:50:02 +0300

    move routine interface and rename routine

--- frontend/src/routines/modal.ts
+++ frontend/src/routines/modal.ts
@@ -1,9 +1,3 @@
-import { ModalTypes } from '../common/enums/ModalTypes';
 import { createRoutine } from 'redux-saga-routines';
 
-export const modal = createRoutine('MODAL');
-
-export interface IModalRoutine{
-  modalType: ModalTypes;
-  show: boolean;
-}
+export const showModalRoutine = createRoutine('MODAL');
