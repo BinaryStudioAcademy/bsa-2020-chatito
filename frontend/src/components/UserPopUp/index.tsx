518c60ac0d35e7cf3c4106e4e3a18bd6d6391fb7
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-08-06 11:35:17 +0300

    code refactor/ style colors added

--- frontend/src/components/UserPopUp/index.tsx
+++ frontend/src/components/UserPopUp/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.sass';
 import React, { FunctionComponent } from 'react';
 import { IUser } from '../../common/models/user';
-import { PopUpPosition } from '../../common/enums/ComponentProps';
+import { blockPosition } from '../../common/types/types';
 import Image from 'react-bootstrap/Image';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
@@ -11,11 +11,10 @@ interface IProps {
   user: IUserState;
   trigger: () => React.ReactElement;
   id: string;
-  placement: PopUpPosition;
+  placement: blockPosition;
 }
 
 const UserPopUp: FunctionComponent<IProps> = ({ user, trigger, id, placement }) => {
-  console.log(user);
   const testUser = {
     fullname: 'test fullname',
     imgUrl: 'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg' // eslint-disable-line max-len
