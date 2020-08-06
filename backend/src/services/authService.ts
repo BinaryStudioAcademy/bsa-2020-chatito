89674fbc060ad4a8f6a4a81f204ee24fca66f1b1
GarryFirst-cripto <vist_01@ukr.net>
2020-08-06 16:12:00 +0300

    Sign-In-flow

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,10 +1,18 @@
 import { getCustomRepository } from 'typeorm';
 
 import UserRepository from '../data/repositories/userRepository';
-import { IRegisterUser } from '../common/models/user/IRegisterUser';
+import { IRegisterUser, IUserId } from '../common/models/user/IRegisterUser';
 import { encrypt } from '../common/utils/encryptHelper';
 import { fromUserToUserClient, fromRegisterUserToCreateUser } from '../common/mappers/user';
 import { createToken } from '../common/utils/tokenHelper';
+
+export const login = async ({ id }: IUserId) => {
+  const logUser = await getCustomRepository(UserRepository).getById(id);
+  return {
+    user: fromUserToUserClient(logUser),
+    token: createToken({ id: logUser.id })
+  };
+};
 
 export const register = async ({ password, ...userData }: IRegisterUser) => {
   const passwordHash = await encrypt(password);
