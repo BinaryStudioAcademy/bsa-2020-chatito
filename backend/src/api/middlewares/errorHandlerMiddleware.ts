15682c5e48e9279582b789fb0f8a8b56a6605d9f
Pavel Sukhinin <psykhinin@ukr.com>
2020-08-07 12:15:32 +0300

    error handling middleware, app.use error handling

---
+++ backend/src/api/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import CustomError from '../../common/models/CustomError';
+
+export default (err: CustomError, _req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    next(err);
+  }
+  console.log(err);
+  res.status(err.status || 500).send(err);
+};
