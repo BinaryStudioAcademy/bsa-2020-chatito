3eeda44ce60c9d965c770111b3677e331d6c9afc
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-08-07 16:17:28 +0300

    renamed Workspace to CreateNewWorkspace scene.

--- frontend/src/scenes/CreateNewWorkspace/index.tsx
+++ frontend/src/scenes/CreateNewWorkspace/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import AddWorkspace from './containers/AddWorkspace';
-import WorkspaceToolbar from './containers/WorkspaceToolbar/index';
 import styles from './styles.module.sass';
 
-const Workspace = () => (
+const CreateNewWorkspace = () => (
   <div className={styles.workspace}>
-    <WorkspaceToolbar />
     <AddWorkspace />
   </div>
 );
 
-export default Workspace;
+export default CreateNewWorkspace;
