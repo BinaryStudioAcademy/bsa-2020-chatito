6812fb82f3cbeb6c6dab4d2d2fb45fae5f587baa
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-08-08 13:00:40 +0300

    add user entity when saving new workspace.

--- backend/src/data/repositories/workspaceRepository.ts
+++ backend/src/data/repositories/workspaceRepository.ts
@@ -1,11 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Workspace } from '../entities/Workspace';
+import { User } from '../entities/User';
 import { ICreateWorkspace } from '../../common/models/workspace/ICreateWorkspace';
 
 @EntityRepository(Workspace)
 class WorkspaceRepository extends Repository<Workspace> {
-  addWorkspace(data: ICreateWorkspace): Promise<Workspace> {
+  addWorkspace(data: ICreateWorkspace, user: User): Promise<Workspace> {
     const workspace = this.create(data);
+    workspace.createdByUser = user;
 
     return workspace.save();
   }
--- backend/src/services/workspaceService.ts
+++ backend/src/services/workspaceService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from 'typeorm';
 
 import WorkspaceRepository from '../data/repositories/workspaceRepository';
+import UserRepository from '../data/repositories/userRepository';
 import { IClientCreateWorkspace } from '../common/models/workspace/IClientCreateWorkspace';
 import { IWorkspaceResponse } from '../common/models/workspace/IWorkspaceResponse';
 import { fromCreatedWorkspaceToClient, fromClientCreateWorkspaceToCreateWorkspace } from '../common/mappers/workspace';
 
 export const createWorkspace = async (data: IClientCreateWorkspace): Promise<IWorkspaceResponse> => {
   const workspaceData = fromClientCreateWorkspaceToCreateWorkspace(data);
+  const user = await getCustomRepository(UserRepository).getById(data.createdByUserId);
 
-  const newWorkspace = await getCustomRepository(WorkspaceRepository).addWorkspace(workspaceData);
+  const newWorkspace = await getCustomRepository(WorkspaceRepository).addWorkspace(workspaceData, user);
 
   return fromCreatedWorkspaceToClient(newWorkspace);
 };
