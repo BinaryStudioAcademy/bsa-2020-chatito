b26ac7b1290033a5ea99b9b3b2bb7c5a7037c707
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-09 02:40:30 +0300

    adding input field with features

--- frontend/src/components/ChangeStatus/index.tsx
+++ frontend/src/components/ChangeStatus/index.tsx
@@ -1,54 +1,82 @@
 import Picker, { IEmojiData } from 'emoji-picker-react';
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
+import { faGrin } from '@fortawesome/free-regular-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './styles.module.sass';
 import EmojiPopUp from '../EmojiPopUp';
-// interface IEmojiClickParams {
-//   event: MouseEvent;
-//   emojiObject: IEmojiData;
-// }
-
-// interface IEmojiClick {
-//   onEmojiClick: (event: MouseEvent, emojiObject: IEmojiData) => void
-// }
+import { InputGroup, FormControl } from 'react-bootstrap';
 
 const ChangeStatus: FunctionComponent = () => {
   const [chosenEmoji, setChosenEmoji] = useState({ emoji: '' });
-  const [showEmojiStatus, setShowEmojiStatus] = useState(false);
+  const [crossStatus, setCrossStatus] = useState(false);
+  const [status, setStatus] = useState('');
   const onEmojiClick = (event: MouseEvent, emojiObject: IEmojiData) => {
     setChosenEmoji(emojiObject);
+    setCrossStatus(true);
   };
-  const showEmojiPopUp = () => {
-    setShowEmojiStatus(!showEmojiStatus);
+  const reset = () => {
+    setChosenEmoji({ emoji: '' });
+    setCrossStatus(false);
+    setStatus('');
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setStatus(e.target.value);
+    console.log(status);
   };
   useEffect(() => {
-    console.log(showEmojiStatus);
-  }, [showEmojiStatus]);
+    console.log(chosenEmoji.emoji);
+  }, [chosenEmoji]);
+  const trigger = () => (
+    <button
+      type="button"
+      className={`${styles.smileButton} ${styles.smileButton_reset}`}
+    >
+      {!chosenEmoji.emoji ? (
+        <FontAwesomeIcon
+          className={styles.smileIcon}
+          icon={faGrin}
+        />
+      ) : (
+        <p>{chosenEmoji.emoji}</p>
+      )}
+    </button>
+  );
   return (
-    <EmojiPopUp />
-    // <div>
-    //   <button
-    //     type="button"
-    //     className={`${styles.arrowButton} ${styles.arrowButton_reset}`}
-    //     onClick={() => showEmojiPopUp()}
-    //   >
-    //     <FontAwesomeIcon
-    //       className={styles.arrowIcon}
-    //       icon={faLocationArrow}
-    //     />
-    //   </button>
-    //   {chosenEmoji ? (
-    //     <span>
-    //       You chose:
-    //       {chosenEmoji.emoji}
-    //     </span>
-    //   ) : (
-    //     <span>No emoji Chosen</span>
-    //   )}
-    //   <Picker onEmojiClick={onEmojiClick} />
-    // </div>
+    <InputGroup className={styles.inputBlock}>
+      <InputGroup.Prepend className={styles.emojiContainer}>
+        <InputGroup.Text className={styles.emoji}>
+          <EmojiPopUp trigger={trigger} onEmojiClick={onEmojiClick} />
+        </InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        placeholder="What is your status?"
+        className={styles.input}
+        type="text"
+        value={status}
+        onChange={onChange}
+      />
+      <InputGroup.Prepend className={styles.emojiContainer}>
+        <InputGroup.Text className={styles.cross}>
+          {crossStatus || status ? (
+            <button
+              type="button"
+              className={`${styles.crossButton} ${styles.crossButton_reset}`}
+              onClick={() => reset()}
+            >
+              <FontAwesomeIcon
+                className={styles.crossIcon}
+                icon={faTimesCircle}
+              />
+            </button>
+          ) : (
+            ''
+          )}
+        </InputGroup.Text>
+      </InputGroup.Prepend>
+    </InputGroup>
   );
 };
 
 export default ChangeStatus;
+// eslint-disable-line max-len
--- frontend/src/components/EmojiPopUp/index.tsx
+++ frontend/src/components/EmojiPopUp/index.tsx
@@ -1,32 +1,19 @@
-import { OverlayTrigger, Button, Image, Popover, Form } from 'react-bootstrap';
+import { OverlayTrigger, Popover, Button, Image, Overlay, Form } from 'react-bootstrap';
 import { IUser } from '../../common/models/user/IUser';
 import Picker, { IEmojiData } from 'emoji-picker-react';
-import React, { FunctionComponent, useState, useEffect } from 'react';
+import React, { FunctionComponent, useState, useEffect, useRef } from 'react';
 import { IBindingCallback1 } from '../../common/models/callback';
 import { ISendMessage } from '../../common/models/message/ISendMessage';
 import styles from './styles.module.sass';
 
 interface IProps {
-  user: IUser;
-  trigger: () => React.ReactElement;
-  id: string;
-  onSend: IBindingCallback1<ISendMessage>;
+  trigger: () => JSX.Element;
+  onEmojiClick: (event: MouseEvent, emojiObject: IEmojiData) => void;
 }
 
-// Mocked data: testData and trigger function
-const testData = {
-  id: '1'
-};
-const trigger = () => <Button variant="success">Show</Button>;
-
-// const ProfilePreview: FunctionComponent<IProps> = ({ user, trigger, id, onSend }) => {
-const EmojiPopUp: FunctionComponent = () => {
-  const [chosenEmoji, setChosenEmoji] = useState({ emoji: '' });
-  const onEmojiClick = (event: MouseEvent, emojiObject: IEmojiData) => {
-    setChosenEmoji(emojiObject);
-  };
+const EmojiPopUp: FunctionComponent<IProps> = ({ onEmojiClick, trigger }) => {
   const showEmoji = (
-    <Popover id={testData.id} className={styles.popUpContainer}>
+    <Popover id="emojiPopUp" className={styles.popUpContainer}>
       <div>
         <Picker onEmojiClick={onEmojiClick} />
       </div>
