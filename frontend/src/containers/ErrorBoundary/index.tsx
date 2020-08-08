7cad70b2b5031d8aecaa22287ec5463c1623caa8
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-08 12:33:51 +0300

    create store for error and connect in to error boundary

--- frontend/src/containers/ErrorBoundary/index.tsx
+++ frontend/src/containers/ErrorBoundary/index.tsx
@@ -1,22 +1,28 @@
-import React, { ErrorInfo } from 'react';
+import React, { ErrorInfo, ReactNode } from 'react';
+import { connect } from 'react-redux';
+import { IErrorBoundaryFormat } from '../../common/models/error/IErrorBoundaryFormat';
+import { setErrorRoutine } from '../../routines/error';
+import { IAppState } from '../../common/models/store';
 
-interface IState {
-  hasError: boolean;
+interface IProps {
+  error?: Error;
+  setError: ({ error, errorInfo }: IErrorBoundaryFormat) => void;
+  children?: ReactNode;
 }
 
-export default class ErrorBoundary extends React.Component<{}, IState> {
-  constructor(props: {}) {
+class ErrorBoundary extends React.Component<IProps> {
+  constructor(props: IProps) {
     super(props);
-    this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true };
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    const { setError } = this.props;
+    setError({ error, errorInfo });
   }
 
   render() {
-    const { hasError } = this.state;
-    if (hasError) {
+    const { error } = this.props;
+    if (error) {
       return <h1 className="d-flex justify-content-end m-3">Ooops, something went wrong...</h1>;
     }
 
@@ -24,3 +30,13 @@ export default class ErrorBoundary extends React.Component<{}, IState> {
     return children;
   }
 }
+
+const mapStateToProps = (state: IAppState) => ({
+  error: state.errorBoundary.error
+});
+
+const mapDispatchToProps = {
+  setError: setErrorRoutine
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ErrorBoundary);
