7e9ba44778b9866499fc98d43419d0e52c3dbc50
Pavel Sukhinin <psykhinin@ukr.com>
2020-08-09 18:32:41 +0300

    input field can accept navigation link

--- frontend/src/components/InputField/InputField.tsx
+++ frontend/src/components/InputField/InputField.tsx
@@ -2,24 +2,44 @@ import React from 'react';
 import { useField } from 'formik';
 import { InputGroup, FormControl } from 'react-bootstrap';
 import styles from './styles.module.sass';
+import { Link } from 'react-router-dom';
 
 interface IProps {
   label: string;
   name: string;
   type: string;
   placeholder?: string;
+  link?: string;
+  linkDescription?: string;
+  linkClassName?: string;
 }
 
-const InputField = ({ label, ...props }: IProps) => {
+const InputField = ({
+  label,
+  link,
+  linkDescription,
+  linkClassName,
+  ...props
+}: IProps) => {
   const [field, meta] = useField(props);
   return (
-    <div className="mb-3">
+    <div className="mb-4 w-100">
+      <div className={styles.labelRow}>
+        <label className={styles.inputLabel} htmlFor={label}>{label}</label>
+        {link
+          ? (
+            <Link
+              className={`${styles.navLink} ${linkClassName}`}
+              to={link}
+            >
+              {linkDescription}
+            </Link>
+          ) : null}
+      </div>
       <InputGroup size="sm" className={styles.inputGroup}>
-        <InputGroup.Prepend>
-          <InputGroup.Text id="inputGroup-sizing-sm">{label}</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl aria-label={label} {...field} {...props} />
+        <FormControl id={label} aria-label={label} {...field} {...props} />
       </InputGroup>
+
       {meta.touched && meta.error ? (
         <div className={`text-danger ${styles.error}`}>{meta.error}</div>
       ) : null}
@@ -28,7 +48,10 @@ const InputField = ({ label, ...props }: IProps) => {
 };
 
 InputField.defaultProps = {
-  placeholder: ''
+  placeholder: '',
+  link: undefined,
+  linkDescription: undefined,
+  linkClassName: undefined
 };
 
 export default InputField;
