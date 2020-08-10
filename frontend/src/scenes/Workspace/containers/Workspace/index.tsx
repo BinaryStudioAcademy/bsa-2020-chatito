2a53af01699053afb6035705180ed3d6a2944fb8
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-10 16:30:50 +0300

    Add profile overview to workspace

--- frontend/src/scenes/Workspace/containers/Workspace/index.tsx
+++ frontend/src/scenes/Workspace/containers/Workspace/index.tsx
@@ -1,34 +1,77 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
+import { connect } from 'react-redux';
 import styles from './styles.module.sass';
 
 import Header from '../Header';
 import WorkspaceToolbar from '../WorkspaceToolbar';
 import ChatScene from '../../../Chat';
+import ProfileOverview from '../../../../components/ProfileOverview';
+import { IUser } from '../../../../common/models/user/IUser';
+import { IAppState } from '../../../../common/models/store';
 
-const Workspace = () => (
-  <div className={styles.mainContainer}>
-    <Header />
-    <div className={styles.contentContainer}>
-      <WorkspaceToolbar />
+export interface IContext {
+  setShowProfileHandler: () => void;
+  setUserDataHandler: (user: IUser | {}) => void;
+}
 
-      <div className={styles.workspaceViewContainer}>
+export const ProfileContext = createContext<IContext | {}>({});
 
-        <div className={styles.LeftPanelWrapper}>
-          <div style={{ background: '#cbcbdc', height: '100%' }}>Left side chats toolbar</div>
-        </div>
+interface IProps {
+  currentUserId: string;
+}
 
-        <div className={styles.ChatWrapper}>
-          <ChatScene />
-        </div>
+const Workspace: React.FC<IProps> = ({ currentUserId }) => {
+  const [showProfile, setShowProfile] = useState(false);
+  const [userData, setUserData] = useState<IUser | {}>({});
 
-        <div className={styles.RightPanelWrapper}>
-          <div style={{ background: '#cbcbdc', height: '100%' }}>Right side toolbar</div>
+  const setShowProfileHandler = () => {
+    setShowProfile(!showProfile);
+  };
+
+  const setUserDataHandler = (user: IUser | {}) => {
+    setUserData(user);
+  };
+
+  const renderProfile = () => (
+    <ProfileOverview
+      user={userData as IUser}
+      currentUserId={currentUserId}
+      setShowProfileHandler={setShowProfileHandler}
+      setUserDataHandler={setUserDataHandler}
+    />
+  );
+
+  return (
+    <div className={styles.mainContainer}>
+      <Header />
+      <div className={styles.contentContainer}>
+        <WorkspaceToolbar />
+
+        <div className={styles.workspaceViewContainer}>
+
+          <div className={styles.LeftPanelWrapper}>
+            <div style={{ background: '#cbcbdc', height: '100%' }}>Left side chats toolbar</div>
+          </div>
+
+          <div className={styles.ChatWrapper}>
+            <ProfileContext.Provider value={{ setShowProfileHandler, setUserDataHandler }}>
+              <ChatScene />
+            </ProfileContext.Provider>
+          </div>
+
+          <div className={styles.RightPanelWrapper}>
+            {showProfile && renderProfile()}
+          </div>
+
         </div>
 
       </div>
-
     </div>
-  </div>
-);
+  );
+};
 
-export default Workspace;
+const mapStateToProps = (state: IAppState) => ({
+  currentUserId: state.user.user
+});
+
+export default connect(mapStateToProps, null)(Workspace);
