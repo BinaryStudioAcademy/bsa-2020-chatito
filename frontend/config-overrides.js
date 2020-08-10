268b65437fcf8730b8186ca94f4ae96ebb8a6cbf
Vyacheslav Dudin <slavak.dudin2@gmail.com>
2020-08-10 13:25:47 +0300

    Try a solution from StackOverflow

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,12 +2,12 @@ const { override, useEslintRc, addWebpackAlias } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
-  useEslintRc(path.resolve(__dirname, '.eslintrc')),
   addWebpackAlias({
     ['@src']: path.resolve(__dirname, 'src'),
     ['@enums']: path.resolve(__dirname, 'src/common/enums'),
     ['@helpers']: path.resolve(__dirname, 'src/common/helpers'),
     ['@models']: path.resolve(__dirname, 'src/common/models'),
     ['@types']: path.resolve(__dirname, 'src/common/types')
-  })
+  }),
+  useEslintRc(path.resolve(__dirname, '.eslintrc'))
 );
