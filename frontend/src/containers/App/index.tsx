bfcad6b86c6c950fff1e74e811d7f2aa559d6896
MoriArchi <vasyadavydov2000@gmail.com>
2020-08-10 18:01:52 +0300

    Remove moks from App container

--- frontend/src/containers/App/index.tsx
+++ frontend/src/containers/App/index.tsx
@@ -1,45 +1,26 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import ReduxToastr from 'react-redux-toastr';
-import { ConnectedRouter } from 'connected-react-router';
 import Routing from '../Routing';
+import { Provider } from 'react-redux';
+import { store } from 'store';
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from 'common/helpers/historyHelper';
 import ErrorBoundary from '../ErrorBoundary';
-import Post from '../../components/Post';
-import { store } from '../../store';
-import { IUser } from '../../common/models/user/IUser';
-import { history } from '../../common/helpers/historyHelper';
-
-const testUser: Partial<IUser> = {
-  fullName: 'Jhonn Smitt',
-  imageUrl: 'https://my.throtl.com/assets/icons/user-default-gray'
-};
+import ReduxToastr from 'react-redux-toastr';
 
 const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Post
-        user={testUser}
-        text="Chatito is a collaboration web platform that allows users to communicate in real-time using easily accessible web interfaces." // eslint-disable-line max-len
-        sendedAt={new Date()}
-      />
+      <ErrorBoundary>
+        <Routing />
+        <ReduxToastr
+          timeOut={3000}
+          position="bottom-right"
+          transitionIn="fadeIn"
+          transitionOut="fadeOut"
+        />
+      </ErrorBoundary>
     </ConnectedRouter>
   </Provider>
 );
-
-// const App = () => (
-//   <Provider store={store}>
-//     <ConnectedRouter history={history}>
-//       <ErrorBoundary>
-//         <Routing />
-//         <ReduxToastr
-//           timeOut={3000}
-//           position="bottom-right"
-//           transitionIn="fadeIn"
-//           transitionOut="fadeOut"
-//         />
-//       </ErrorBoundary>
-//     </ConnectedRouter>
-//   </Provider>
-// );
 
 export default App;
