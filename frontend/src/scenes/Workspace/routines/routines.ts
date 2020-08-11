89ca257a9c5367195467c13d6baa85442c73725e
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-11 15:15:09 +0300

    change workspace scene files name

--- frontend/src/scenes/Workspace/routines/routines.ts
+++
@@ -1,4 +0,0 @@
-import { createRoutine } from 'redux-saga-routines';
-
-export const addWorkspaceRoutine = createRoutine('ADD_WORKSPACE');
-export const selectChatRoutine = createRoutine('SELECT_CHAT');
