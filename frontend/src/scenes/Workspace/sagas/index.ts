3766f10282d9e9337baca5492dc8961dcf69d6d6
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-11 15:17:04 +0300

    connect workspace saga to root saga

---
+++ frontend/src/scenes/Workspace/sagas/index.ts
@@ -0,0 +1,25 @@
+import { addWorkspaceRoutine } from 'scenes/Workspace/routines';
+import { Routine } from 'redux-saga-routines';
+import { takeEvery, put, call, all } from 'redux-saga/effects';
+import { toastr } from 'react-redux-toastr';
+import { addWorkspace } from 'services/workspaceService';
+
+function* addWorkspaceReq({ payload }: Routine<any>) {
+  try {
+    const workspace = yield call(addWorkspace, payload);
+    yield put(addWorkspaceRoutine.success(workspace));
+  } catch (error) {
+    yield call(toastr.error, 'Error', error.message);
+    yield put(addWorkspaceRoutine.failure(error));
+  }
+}
+
+function* watchPostWorkspaceName() {
+  yield takeEvery(addWorkspaceRoutine.TRIGGER, addWorkspaceReq);
+}
+
+export default function* workspaceSaga() {
+  yield all([
+    watchPostWorkspaceName()
+  ]);
+}
