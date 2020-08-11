99a07b4aa250ea65659e4e50a578f811f088df65
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-11 20:01:10 +0300

    pulling data

---
+++ frontend/build/precache-manifest.59d609dd0b095a492659a52c9275e0d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3284c8c731799360624d74b23c909d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e1f05a786a2cfea597f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c65a5b47564bf174db0b",
+    "url": "/static/css/main.d64d3825.chunk.css"
+  },
+  {
+    "revision": "6e1f05a786a2cfea597f",
+    "url": "/static/js/2.42863965.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.42863965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65a5b47564bf174db0b",
+    "url": "/static/js/main.5d3efa60.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "cf8555f0ca82b6079963ac35fe7830b3",
+    "url": "/static/media/bg-img.cf8555f0.png"
+  },
+  {
+    "revision": "06a85bf10f536816240e6276efa6c4fe",
+    "url": "/static/media/chatitoMascot.06a85bf1.svg"
+  },
+  {
+    "revision": "c682839b645d3b36fdf825125819367f",
+    "url": "/static/media/logo.c682839b.svg"
+  },
+  {
+    "revision": "70a2507b1b37f667b63a30b9b5f48454",
+    "url": "/static/media/signInFacebook.70a2507b.svg"
+  },
+  {
+    "revision": "920afcae1d540bc7996ecd69ce33b859",
+    "url": "/static/media/signInGoogle.920afcae.svg"
+  }
+]);
