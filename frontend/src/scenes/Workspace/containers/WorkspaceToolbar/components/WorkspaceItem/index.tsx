899b44c7587b9072e0d4cfe5310fd9f6ca753bf3
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-11 15:19:01 +0300

    edit workspace toolbar container, connect it to store

--- frontend/src/scenes/Workspace/containers/WorkspaceToolbar/components/WorkspaceItem/index.tsx
+++ frontend/src/scenes/Workspace/containers/WorkspaceToolbar/components/WorkspaceItem/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.sass';
+import { ListGroup } from 'react-bootstrap';
 
 interface IWorkspace {
   [key: string]: string;
@@ -7,22 +8,28 @@ interface IWorkspace {
 
 interface IProps {
   workspace: IWorkspace;
-  id: string;
+  isSelected: boolean;
+  onItemClick: () => void;
 }
 
-export default function WorkspaceItem({ workspace }: IProps) {
-  const currentId = '2';
+export default function WorkspaceItem({
+  workspace,
+  isSelected,
+  onItemClick
+}: IProps) {
   return (
-    <div className={styles.workspaceItemBlock}>
-      {currentId === workspace.id ? (
-        <div className={`${styles.imageContainer} ${styles.activeWorkspace}`}>
-          <img className={styles.item} src={workspace.imgUrl} alt="workspaceItem" />
-        </div>
-      ) : (
-        <div className={styles.imageContainer}>
-          <img className={styles.item} src={workspace.imgUrl} alt="workspaceItem" />
-        </div>
-      )}
-    </div>
+    <ListGroup.Item
+      onClick={onItemClick}
+      className={isSelected ? styles.selectedItem : styles.item}
+    >
+      <div className={isSelected ? styles.selectedAvatar : styles.avatar}>
+        {workspace.imageUrl ? (
+          <img className={styles.itemWithAvatar} src={workspace.imageUrl} alt="WorkspaceItem" />
+        ) : (
+          <span className={styles.itemWithoutAvatar}>{workspace.name[0]}</span>
+        )}
+      </div>
+      <span className={isSelected ? styles.selectedItemName : styles.itemName}>{workspace.name}</span>
+    </ListGroup.Item>
   );
 }
