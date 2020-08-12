e95d6d115b8060f1aee8229d99956fb34c3b8d9b
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-12 11:29:30 +0300

    Add post reaction routes

--- backend/src/api/routes/postRoutes.ts
+++ backend/src/api/routes/postRoutes.ts
@@ -1,11 +1,15 @@
 import { Router, Request } from 'express';
 import { run } from '../../common/utils/routeHelper';
-import { addPost, editPost } from '../../services/postService';
+import { addPost, editPost, addReaction,
+  updateReaction, deleteReaction } from '../../services/postService';
 
 const router = Router();
 
 router
   .post('/', run((req: Request) => addPost(req.user.id, req.body)))
-  .put('/', run((req: Request) => editPost(req.body)));
+  .put('/', run((req: Request) => editPost(req.body)))
+  .post('/react', run((req: Request) => addReaction(req.body.reaction, req.body.postId, req.user.id)))
+  .put('/react', run((req: Request) => updateReaction(req.body.id, req.body.reaction)))
+  .delete('/react', run((req: Request) => deleteReaction(req.body.id)));
 
 export default router;
