96e593d04ed175308c3992fbcf050476bbd56368
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-12 11:27:14 +0300

    Add service methods

--- backend/src/services/postService.ts
+++ backend/src/services/postService.ts
@@ -1,9 +1,10 @@
 import { getCustomRepository } from 'typeorm';
 import PostRepository from '../data/repositories/postRepository';
+import UserRepository from '../data/repositories/userRepository';
+import PostReactionRepository from '../data/repositories/postReactionRepository';
 import { ICreatePost } from '../common/models/post/ICreatePost';
 import { IEditPost } from '../common/models/post/IEditPost';
 import { IPost } from '../common/models/post/IPost';
-import UserRepository from '../data/repositories/userRepository';
 
 export const addPost = async (id: string, post: ICreatePost) => {
   const user = await getCustomRepository(UserRepository).getById(id);
@@ -18,3 +19,19 @@ export const editPost = async ({ id, text }: IEditPost) => {
   const editedPost: IPost = await getCustomRepository(PostRepository).editPost(id, text);
   return editedPost;
 };
+
+export const addReaction = (reaction: string, postId: string, userId: string) => (
+  getCustomRepository(PostReactionRepository).addReaction({
+    reaction,
+    post: { id: postId },
+    user: { id: userId }
+  })
+);
+
+export const updateReaction = (id: string, reaction: string) => (
+  getCustomRepository(PostReactionRepository).updateReaction(id, reaction)
+);
+
+export const deleteReaction = (id: string) => (
+  getCustomRepository(PostReactionRepository).deleteReaction(id)
+);
