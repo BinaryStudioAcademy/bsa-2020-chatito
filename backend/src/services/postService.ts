cb58417992f43d642a20ac6a02903d90245de1ae
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-13 22:12:55 +0300

    emiting when edit/create post

--- backend/src/services/postService.ts
+++ backend/src/services/postService.ts
@@ -9,8 +9,8 @@ import ChatRepository from '../data/repositories/chatRepository';
 import { fromPostToPostClient } from '../common/mappers/post';
 import CommentRepository from '../data/repositories/commentRepository';
 import { fromPostCommentsToPostCommentsClient } from '../common/mappers/comment';
-import { emitToRoom } from '../common/utils/socketHelper';
 import { SocketRoutes } from '../common/enums/SocketRoutes';
+import { emitToChatRoom } from '../common/utils/socketHelper';
 
 export const addPost = async (id: string, post: ICreatePost) => {
   const user = await getCustomRepository(UserRepository).getById(id);
@@ -18,14 +18,14 @@ export const addPost = async (id: string, post: ICreatePost) => {
   const newPost: ICreatePost = { ...post, createdByUser: user, chat };
   const createdPost: IPost = await getCustomRepository(PostRepository).addPost(newPost);
   const clientPost = await fromPostToPostClient(createdPost);
-  emitToRoom(clientPost.chatId, SocketRoutes.AddPost, createdPost);
+  emitToChatRoom(clientPost.chatId, SocketRoutes.AddPost, clientPost);
   return clientPost;
 };
 
 export const editPost = async ({ id, text }: IEditPost) => {
   const editedPost: IPost = await getCustomRepository(PostRepository).editPost(id, text);
   const clientPost = await fromPostToPostClient(editedPost);
-  emitToRoom(clientPost.chatId, SocketRoutes.EditPost, clientPost);
+  emitToChatRoom(clientPost.chatId, SocketRoutes.EditPost, clientPost);
   return clientPost;
 };
 
