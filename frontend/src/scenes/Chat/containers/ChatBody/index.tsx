49bb2f9cfb1266f040b55b3ed40176e69669feef
Pavel Sukhinin <psykhinin@ukr.com>
2020-08-13 09:47:06 +0300

    post mock

--- frontend/src/scenes/Chat/containers/ChatBody/index.tsx
+++ frontend/src/scenes/Chat/containers/ChatBody/index.tsx
@@ -14,6 +14,20 @@ interface IProps {
   openThread: IBindingCallback1<IPost>;
   activeThreadPostId: string | undefined;
 }
+const postMock = {
+  user: {
+    id: '87d8eee5-6655-422a-a0b8-05a4709b626c',
+    fullName: 'demo',
+    email: 'demo10@demo.com',
+    displayName: 'demo',
+    imageUrl: 'https://images.unsplash.com/photo-1555445091-5a8b655e8a4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', // eslint-disable-line max-len
+    title: 'string',
+    status: 'online'
+  },
+  id: 'e4bf2dab-1fc1-4c9b-a75e-67c23418d9c2',
+  text: 'Phasellus varius pretium vulputate. Sed finibus maximus tortor, ac cursus nulla dignissim a. Mauris iaculis, turpis vel pulvinar iaculis, ex ipsum rhoncus erat, sit amet convallis turpis libero posuere urna. Donec orci nulla, ultrices sit amet enim quis, suscipit posuere eros. Aenean sed magna diam. Donec imperdiet sagittis nulla, elementum feugiat sapien maximus quis. Nulla nec rhoncus urna. Suspendisse mi dui, sollicitudin sit amet ante ut, semper malesuada nisi.', // eslint-disable-line
+  createdAt: '2020-08-11T12:40:36.072Z'
+};
 
 const ChatBody: React.FC<IProps> = ({ messages, openProfile, openThread, activeThreadPostId = '' }) => {
   const handleOpenThread = (post: IPost) => {
@@ -23,7 +37,8 @@ const ChatBody: React.FC<IProps> = ({ messages, openProfile, openThread, activeT
 
   return (
     <div className={styles.chatBody}>
-      {messages.map(m => <Post post={m} openThread={handleOpenThread} openUserProfile={openProfile} />)}
+      <Post post={postMock} openThread={handleOpenThread} openUserProfile={openProfile} />
+      {/* {messages.map(m => <Post post={m} openThread={handleOpenThread} openUserProfile={openProfile} />)} */}
     </div>
   );
 };
