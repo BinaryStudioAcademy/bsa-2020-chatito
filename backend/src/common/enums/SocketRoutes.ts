b241c66853d62bf50083fc55eb66f85cfe0dd701
AlexanderTirik <sasha.tirik@gmail.com>
2020-08-13 13:54:07 +0300

    connect sockets to the server

---
+++ backend/src/common/enums/SocketRoutes.ts
@@ -0,0 +1,4 @@
+export enum SocketRoutes {
+  NewPost = 'NEW_POST',
+  EditPost = 'EDIT_POST'
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,16 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import passport from 'passport';
 import { createConnection } from 'typeorm';
+import socketIO from 'socket.io';
 import { env } from './env';
 import routes from './api/routes';
 import authorizationMiddleware from './api/middlewares/authorizationMiddleware';
+import jwtSocketMiddleware from './api/middlewares/socketAuthorizationMiddleware';
 import errorHandlerMiddleware from './api/middlewares/errorHandlerMiddleware';
 import routesWhiteList from './config/routesWhiteListConfig';
 import './config/passportConfig';
 import './config/sendgridConfig';
+import { initJoinToChatRoom } from './services/chatService';
 
 const app = express();
 
@@ -25,13 +28,23 @@ routes(app);
 const { port } = env.app;
 
 app.use(errorHandlerMiddleware);
-app.listen(port, async () => {
+
+const server = app.listen(port, async () => {
   try {
     await createConnection();
     console.log(`Server is running at ${port}.`); // eslint-disable-line no-console
   } catch (error) {
     console.log('App started with error:', error); // eslint-disable-line no-console
   }
+});
+
+export const io = socketIO().listen(server);
+
+io.use(jwtSocketMiddleware);
+
+io.on('connection', socket => {
+  const { user } = socket.request;
+  initJoinToChatRoom(socket, user.id);
 });
 
 export default app;
