b5efeb775066304a085cafa9f313765af1ddffba
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-08-20 22:44:49 +0300

    fix bug with loading chats with drafts (due to used service change)

--- backend/src/data/repositories/chatRepository.ts
+++ backend/src/data/repositories/chatRepository.ts
@@ -19,13 +19,39 @@ class ChatRepository extends Repository<Chat> {
     return this.findOne({ where: { id }, relations: ['posts', 'users', 'workspace'] });
   }
 
-  getAllByWorkspaceIdAndUserId(workspaceId: string, userId: string) {
-    return this.createQueryBuilder('chat')
-      .leftJoinAndSelect('chat.users', 'users')
-      .where('users.id = :userId', { userId })
-      .leftJoinAndSelect('chat.workspace', 'workspace')
-      .where('workspace.id = :workspaceId', { workspaceId })
+  async getAllByWorkspaceIdAndUserId(workspaceId: string, userId: string) {
+    const chats = await this.createQueryBuilder('chat')
+      .select([
+        'chat.id',
+        'chat.name',
+        'chat.type',
+        'chat.hash',
+        'chat.isPrivate',
+        'user.id',
+        'user.imageUrl',
+        'draft_post.id',
+        'draft_post.text'
+      ])
+      .leftJoin(
+        'chat.workspace',
+        'workspace',
+        'workspace.id = :workspaceId',
+        { workspaceId }
+      )
+      .leftJoin(
+        'chat.draftPosts',
+        'draft_post',
+        'draft_post."chatId" = chat.id AND draft_post."createdByUserId" = :userId',
+        { userId }
+      )
+      .leftJoin(
+        'chat.users',
+        'user'
+      )
+      .where('chat."createdByUserId" = :userId', { userId })
       .getMany();
+
+    return chats;
   }
 
   async getAllByUser(userId: string): Promise<any> {
