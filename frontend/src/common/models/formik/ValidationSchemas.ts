1f4659eacdbf97c4a0f01c9c4f44577723c1789e
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-20 10:45:24 +0300

    changes according to comment

--- frontend/src/common/models/formik/ValidationSchemas.ts
+++ frontend/src/common/models/formik/ValidationSchemas.ts
@@ -1,88 +1,63 @@
 import * as Yup from 'yup';
 
+const password = Yup.string()
+  .min(8, 'Password must be at least 8 characters')
+  .required('Password is required')
+  .matches(
+    /^.*((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+    'Password must include both uppercase and lowercase letters'
+  )
+  .matches(
+    /^.*(?=.*\d).*$/,
+    'Password must include numbers'
+  )
+  .matches(
+    /^.*((?=.*[!/@#$%^&*()\-_=+{};:,<.>]){1}).*$/,
+    'Password must include special symbols'
+  );
+
+const email = Yup.string()
+  .email('Email is invalid')
+  .required('Email is required');
+
+const confirmPassword = Yup.string()
+  .oneOf([Yup.ref('password'), ''], 'Passwords must match')
+  .required('Confirm Password is required');
+
+const workspaceName = Yup.string()
+  .required('Workspace name is required')
+  .min(5, 'Enter more, than 5 symbols')
+  .max(40, 'Workspace name should be no longer 40 characters');
+
+const fullName = Yup.string()
+  .required('Full Name is required')
+  .max(100, 'Full name should be no longer 100 characters');
+
 export const signInValSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Email is invalid')
-    .required('Email is required'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required')
-    .matches(
-      /^.*((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      'Password must include both uppercase and lowercase letters'
-    )
-    .matches(
-      /^.*(?=.*\d).*$/,
-      'Password must include numbers'
-    )
-    .matches(
-      /^.*((?=.*[!/@#$%^&*()\-_=+{};:,<.>]){1}).*$/,
-      'Password must include special symbols'
-    )
+  email,
+  password
 });
+
 export const signUpValSchema = Yup.object().shape({
-  fullName: Yup.string()
-    .required('Full Name is required')
-    .max(100, 'Full name should be no longer 100 characters'),
-  email: Yup.string()
-    .email('Email is invalid')
-    .required('Email is required'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required')
-    .matches(
-      /^.*((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      'Password must include both uppercase and lowercase letters'
-    )
-    .matches(
-      /^.*(?=.*\d).*$/,
-      'Password must include numbers'
-    )
-    .matches(
-      /^.*((?=.*[!/@#$%^&*()\-_=+{};:,<.>]){1}).*$/,
-      'Password must include special symbols'
-    ),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), ''], 'Passwords must match')
-    .required('Confirm Password is required')
+  fullName,
+  email,
+  password,
+  confirmPassword
 });
 
 export const forgotPasswordSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Email is invalid')
-    .required('Email is required')
+  email
 });
 
 export const resetPasswordSchema = Yup.object().shape({
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required')
-    .matches(
-      /^.*((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      'Password must include both uppercase and lowercase letters'
-    )
-    .matches(
-      /^.*(?=.*\d).*$/,
-      'Password must include numbers'
-    )
-    .matches(
-      /^.*((?=.*[!/@#$%^&*()\-_=+{};:,<.>]){1}).*$/,
-      'Password must include special symbols'
-    ),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), ''], 'Passwords must match')
-    .required('Confirm Password is required')
+  password,
+  confirmPassword
 });
 
 export const inviteLinkSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Email is invalid')
-    .required('Email is required')
+  email
 });
 
 export const createWorkspaceValSchema = Yup.object().shape({
-  workspaceName: Yup.string()
-    .required('Workspace name is required')
-    .min(5, 'Enter more, than 5 symbols')
-    .max(40, 'Workspace name should be no longer 40 characters')
+  workspaceName
 });
