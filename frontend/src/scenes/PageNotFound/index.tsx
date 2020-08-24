1d8c9a6340922976054a861ff10165a38d76d62d
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-24 15:11:46 +0300

    Add header to PageNotFound

--- frontend/src/scenes/PageNotFound/index.tsx
+++ frontend/src/scenes/PageNotFound/index.tsx
@@ -3,30 +3,34 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.sass';
 import { ReactComponent as Upset } from 'img/error404.svg';
 import { Button } from 'react-bootstrap';
+import Header from 'scenes/Workspace/containers/Header';
 
 const PageNotFound: FunctionComponent = () => (
-  <div className={styles.notFoundPage}>
-    <div className={styles.centralBlock}>
-      <Upset className={styles.upset} />
-      <div className={styles.messageContainer}>
-        <div className={styles.text}>
-          <p>OOPS!</p>
-          <p>Page not found</p>
-        </div>
-        <div className={`${styles.formFooter} w-100`}>
-          <Link to="/">
-            <Button
-              type="submit"
-              variant="primary"
-              className={styles.primaryBtn}
-            >
-              Go to Homepage
-            </Button>
-          </Link>
+  <>
+    <Header />
+    <div className={styles.notFoundPage}>
+      <div className={styles.centralBlock}>
+        <Upset className={styles.upset} />
+        <div className={styles.messageContainer}>
+          <div className={styles.text}>
+            <p>OOPS!</p>
+            <p>Page not found</p>
+          </div>
+          <div className={`${styles.formFooter} w-100`}>
+            <Link to="/">
+              <Button
+                type="submit"
+                variant="primary"
+                className={styles.primaryBtn}
+              >
+                Go to Homepage
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default PageNotFound;
