1582060902342e0d8f8b91b9bab0871fd3447ef4
Dmytro Potapenko <1lindern1@gmail.com>
2020-08-25 12:27:08 +0300

    solving conflicts2

--- frontend/src/scenes/Chat/containers/InviteChatModal/index.tsx
+++ frontend/src/scenes/Chat/containers/InviteChatModal/index.tsx
@@ -22,80 +22,6 @@ const InviteChatModal: React.FC<IProps> = ({ chatName, chatId, chatUsers,
     toggleModal({ modalType: ModalTypes.InviteChat, show: !isShown });
   };
 
-<<<<<<< HEAD
-  const onSelectUser = (user: IUser) => {
-    if (chatUsers.find(chatUser => chatUser.id === user.id)) {
-      return;
-    }
-    if (!selectedUsers.find(selUser => selUser.id === user.id)) {
-      setSelectedUsers(state => [...state, user]);
-    }
-    setText('');
-  };
-
-  const onUnselectUser = (id: string) => {
-    setSelectedUsers(state => [...state.filter(user => user.id !== id)]);
-  };
-
-  const onAdd = () => {
-    const userIds = selectedUsers.map(user => user.id);
-    addUsersToChat({ chatId, userIds });
-
-    onHide();
-  };
-
-  const popover = (
-    <Popover id="popover-basic" className={styles.popover}>
-      <Popover.Content className={styles.content}>
-        {searchedUsers.length
-          ? searchedUsers.map(user => {
-            const isUserInChat = !!chatUsers.find(chatUser => chatUser.id === user.id);
-            const statusCls = [styles.status, styles.online];
-            const btnCls = [isUserInChat ? styles.chatUser : '', styles.userItem, 'button-unstyled', 'noselect'];
-            return (
-              <button className={btnCls.join(' ')} onClick={() => onSelectUser(user)} type="button" key={user.id}>
-                <span className={styles.fullName}>{user.fullName}</span>
-                <i className={statusCls.join(' ')} />
-                <span className={styles.displayName}>{user.displayName}</span>
-                {isUserInChat && <span className={styles.additional}>Already in this channel</span>}
-              </button>
-            );
-          })
-          : (
-            <div className={styles.notFound}>
-              <span>No one found matching</span>
-              <strong>{text}</strong>
-            </div>
-          )}
-      </Popover.Content>
-    </Popover>
-  );
-
-  return (
-    <ModalWindow isShown={isShown} onHide={onHide}>
-      <div className={styles.modalWrp}>
-        <h4 className={styles.title}>Add people</h4>
-        <p className={styles.chatName}>{chatName}</p>
-        <div className={styles.selectedUsersWrp}>
-          {selectedUsers.map(selUser => (
-            <div className={styles.selectedUser} key={selUser.id}>
-              <div className={`${styles.selectedUsername} noselect`}>{selUser.fullName}</div>
-              <FontAwesomeIcon onClick={() => onUnselectUser(selUser.id)} icon={faTimes} className={styles.closeBtn} />
-            </div>
-          ))}
-        </div>
-        <LoaderWrapper loading={loading} height="50px">
-          <OverlayTrigger trigger="focus" overlay={popover} placement="bottom" show={!!text}>
-            <InputGroup className="mb-3">
-              <FormControl placeholder="Search by name or email" onChange={onChange} value={text} />
-            </InputGroup>
-          </OverlayTrigger>
-        </LoaderWrapper>
-        <Button variant="primary" disabled={!selectedUsers.length} className={styles.addBtn} onClick={onAdd}>
-          Add
-        </Button>
-      </div>
-=======
   return (
     <ModalWindow isShown={!postsLoading && isShown} onHide={onHide}>
       <InviteChatForm
@@ -104,7 +30,6 @@ const InviteChatModal: React.FC<IProps> = ({ chatName, chatId, chatUsers,
         chatId={chatId}
         chatUsers={chatUsers}
       />
->>>>>>> bug/loader
     </ModalWindow>
   );
 };
