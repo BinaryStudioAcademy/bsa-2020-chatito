bd5a1b4d29d241f321d807153c6000e979d3aae0
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-08-31 17:55:22 +0300

    Fix region issue

--- backend/src/config/awsConfig.ts
+++ backend/src/config/awsConfig.ts
@@ -1,8 +1,7 @@
 import aws from 'aws-sdk';
 import { env } from '../env';
 
-export const { accessKeyId, secretAccessKey, bucket, url: AWSUrl } = env.aws;
+export const { accessKeyId, secretAccessKey, bucket, url: AWSUrl, region } = env.aws;
 export const signedUrlExpireSeconds = 500;
-export const region = 'eu-central-1';
 
 export const s3 = new aws.S3({ accessKeyId, secretAccessKey, region, signatureVersion: 'v4' });
