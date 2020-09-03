6231f31b05a39158ce07f17af4e55dc8d8bf3361
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-09-03 16:00:12 +0300

    hook added

--- backend/src/services/hooksService.ts
+++ backend/src/services/hooksService.ts
@@ -1,6 +1,7 @@
 import { addPost } from './postService';
 import { getGithubUser } from './userService';
 import { fromGithubPayloadToPost } from '../common/mappers/hooks';
+import { getWorkspaceByName } from './workspaceService';
 
 export const addGithubNotification = async (githubPayload: any) => {
   const post = await fromGithubPayloadToPost(githubPayload);
@@ -11,8 +12,15 @@ export const addGithubNotification = async (githubPayload: any) => {
   return notificationPost;
 };
 
-export const sendScheduliaMessage = async (payload: any): null => {
+export const sendScheduliaMessage = async (payload: any): Promise<null> => {
+  // PAYLOAD
+  // workspace: workspace name
+  // users: [] of user emails
+  // message: optional string
+
   console.log('schedulia');
   console.log(payload);
+  const wp = getWorkspaceByName(payload.workspace);
+  // Check if users have common chats
   return null;
 };
