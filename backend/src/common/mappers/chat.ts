d12184c14da8a789409646d5f76f4977bad6206d
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-09-07 16:57:00 +0300

    add chat description

--- backend/src/common/mappers/chat.ts
+++ backend/src/common/mappers/chat.ts
@@ -2,12 +2,13 @@ import { Chat } from '../../data/entities/Chat';
 import { fromUserToUserClient } from './user';
 
 export const fromChatToClientChat = (chat: Chat) => {
-  const { id, name, type, isPrivate, workspaceId, createdByUserId, hash, users, draftPosts } = chat;
+  const { id, name, type, description, isPrivate, workspaceId, createdByUserId, hash, users, draftPosts } = chat;
   const clientUsers = users.map(user => fromUserToUserClient(user));
   return {
     id,
     name,
     type,
+    description,
     isPrivate,
     workspaceId,
     createdByUserId,
