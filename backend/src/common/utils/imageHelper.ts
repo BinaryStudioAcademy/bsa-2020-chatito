0899ef9cc388e78c03e0ce84d86d16a207a38e16
Dmytro Potapenko <1lindern1@gmail.com>
2020-09-07 23:39:00 +0300

    fixed

--- backend/src/common/utils/imageHelper.ts
+++ backend/src/common/utils/imageHelper.ts
@@ -1,5 +1,11 @@
 import { AWSUrl } from '../../config/awsConfig';
 
-export const getImageUrl = (imageUrl: string) => (
-  imageUrl ? `${AWSUrl}${imageUrl}` : null
-);
+export const getImageUrl = (imageUrl: string) => {
+  if (AWSUrl === 'url') {
+    return 'https://bsa-chatito-storage.s3.amazonaws.com/assets/defaultUserImage.png';
+  }
+  if (imageUrl) {
+    return `${AWSUrl}${imageUrl}`;
+  }
+  return null;
+};
