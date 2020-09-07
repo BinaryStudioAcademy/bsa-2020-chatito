df9e1349c202d00a9e5fd23a3822705adafcca9e
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-09-07 10:52:27 +0300

    cors protection added

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,4 +1,10 @@
+const whitelist = ['http://dev.schedulia.xyz', 'dev.schedulia.xyz'];
 export const ScheduliaIntegrationOptions = {
-  origin: 'http://dev.schedulia.xyz',
-  optionsSuccessStatus: 200
+  origin: (origin: any, callback: (err: Error, isAllowed?: boolean) => any) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 };
