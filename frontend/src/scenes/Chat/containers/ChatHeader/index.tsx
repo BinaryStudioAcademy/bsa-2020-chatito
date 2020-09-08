979effd1f95fa64197730a97deb34bfd8fb18291
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2020-09-08 17:47:59 +0300

    removed info from direcs

--- frontend/src/scenes/Chat/containers/ChatHeader/index.tsx
+++ frontend/src/scenes/Chat/containers/ChatHeader/index.tsx
@@ -141,16 +141,18 @@ const ChatHeader: React.FC<IProps> = ({ chat, showModal, setMute, currentUser, s
           ) : (
             <div className={styles.title}>{chatName}</div>
           )}
-
-          <OverlayTrigger
-            trigger={['hover', 'hover']}
-            delay={{ show: 300, hide: 0 }}
-            rootClose
-            placement="bottom-start"
-            overlay={PopoverItem(chat.description || 'No description for this chat')}
-          >
-            <FontAwesomeIcon icon={faInfoCircle} className={styles.icon} />
-          </OverlayTrigger>
+          { chat.type === ChatType.Channel
+            && (
+            <OverlayTrigger
+              trigger={['hover', 'hover']}
+              delay={{ show: 300, hide: 0 }}
+              rootClose
+              placement="bottom-start"
+              overlay={PopoverItem(chat.description || 'No description for this chat')}
+            >
+              <FontAwesomeIcon icon={faInfoCircle} className={styles.icon} />
+            </OverlayTrigger>
+            )}
 
           <FontAwesomeIcon icon={faStar} className={styles.icon} />
 
