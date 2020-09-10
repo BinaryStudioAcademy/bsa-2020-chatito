cf75efe7e0f5f2da65d1db948fcafa2699275aeb
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-09-10 12:57:03 +0300

    Fix facebook avatar

--- backend/src/common/utils/imageHelper.ts
+++ backend/src/common/utils/imageHelper.ts
@@ -1,5 +1,13 @@
 import { AWSUrl } from '../../config/awsConfig';
 
-export const getImageUrl = (imageUrl: string) => (
-  imageUrl ? `${AWSUrl}${imageUrl}` : null
-);
+const r = new RegExp('^(?:[a-z]+:)?//', 'i');
+
+export const getImageUrl = (imageUrl: string) => {
+  if (r.test(imageUrl)) {
+    return imageUrl;
+  }
+  if (imageUrl) {
+    return `${AWSUrl}${imageUrl}`;
+  }
+  return null;
+};
